269763629dc00d69b85908bcff9136cd509190f5
Ashutosh-Maurya-87 <ashumaurya486@gmail.com>
2021-09-22 18:45:42 +0530

    'some-addition'

--- src/component/Sign.jsx
+++ src/component/Sign.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "@mui/material";
 import "../component/style.css";
+import banner from "./imeges/login-bannerimg.png";
 
 const Sign = () => {
+  const [userName, setUserName] = useState("");
+  const [userPass, setUserPass] = useState(" ");
   const btnFunc = () => {
+    alert("data are save");
     console.log("this button work ");
+    setUserName(" ");
+    setUserPass(" ");
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit function call");
   };
 
   return (
@@ -13,38 +24,43 @@ const Sign = () => {
         <div className="row">
           <div className="col-sm-10">
             Don't have an account?<Link>Sign Up</Link>
-            <br />
-            <br />
-            <br />
-            <br />
-            <p style={{ fontSize: "50px" }}>
+            <p className="sign-text">
               Sign in to <b>Nsestb</b>
             </p>
-            <form>
+            <form onSubmit={onSubmit}>
               <label style={{ color: "gray", fontWeight: "bold" }}>
                 ENTER YOUR USERNAME
               </label>
               <input
-                style={{ width: "80%", height: "50px" }}
+                style={{ width: "100%", height: "50px" }}
                 type="text"
                 placeholder="e.g. ashumaurya"
+                value={userName}
+                onChange={(e) => {
+                  setUserName(e.target.value);
+                }}
               />
-              <label style={{ color: "gray", fontWeight: "bold" }}>
+              <label
+                style={{ color: "gray", fontWeight: "bold", marginTop: "5%" }}
+              >
                 ENTER YOUR PASSWORD
               </label>
               <input
-                style={{ width: "80%", height: "50px" }}
+                style={{ width: "100%", height: "50px" }}
                 type="password"
+                value={userPass}
+                onChange={(e) => {
+                  setUserPass(e.target.value);
+                }}
                 placeholder="enter your password"
               />
+              <input type="checkbox" />
+              <label>keep me logged in next time</label>
               <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <span>Forget Password</span>
+
+              <p className="text">Forget Password?</p>
               <button
-                type="button"
+                type="submit"
                 className="btn btn-primary "
                 onClick={btnFunc}
               >
@@ -53,6 +69,9 @@ const Sign = () => {
             </form>
           </div>
         </div>
+      </div>
+      <div className="col-sm-4 text-right">
+        <img src={banner} alt="" />
       </div>
     </>
   );
