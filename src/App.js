892456a6dc177ef12a39a1b78f5107e5e8dcf3ea
Ashutosh-Maurya-87 <ashumaurya486@gmail.com>
2021-09-22 11:47:04 +0530

    'some-addition'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import "./App.css";
 import { Link } from "@mui/material";
-
+import "./component/style.css";
 function App() {
   return (
     <>
       <h3 className="text-center">Welcome</h3>
-      <button type="button" className="btn btn-primary" variant="primary">
-        Login
-      </button>
+
       <div className="container">
         <div className="row">
           <div className="col-sm-10">
             Don't have an account?<Link>Sign Up</Link>
+            <br />
+            <br />
+            <br />
+            <br />
             <p style={{ fontSize: "50px" }}>
               Sign in to <b>Nsestb</b>
             </p>
-            <b>ENTER YOUR USERNAME</b> <br />
+            <b style={{ color: "gray" }}>ENTER YOUR USERNAME</b> <br />
             <input
-              style={{ width: "50%", height: "50px" }}
+              style={{ width: "80%", height: "50px" }}
               type="text"
               placeholder="e.g. ashumaurya"
             />
@@ -25,15 +27,23 @@ function App() {
             <b>ENTER YOUR PASSWORD</b>
             <br />
             <input
-              style={{ width: "50%", height: "50px" }}
+              style={{ width: "80%", height: "50px" }}
               type="password"
               placeholder="enter your password"
             />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <button type="button" className="btn btn-primary" variant="primary">
+              Login
+            </button>
           </div>
-          {/* <div className="col-lg-5">
-            <h2>colunm2</h2>
-            <p>lorem epsum sdfgds dsgsdg rety ersty ewrwert asrsafsd bg</p>
-          </div> */}
+        </div>
+        <div className="col-lg-5">
+          <h2>colunm2</h2>
+          <p>lorem epsum sdfgds dsgsdg rety ersty ewrwert asrsafsd bg</p>
         </div>
       </div>
     </>
